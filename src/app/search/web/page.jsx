33bc3d2db32d9118e09a449525980d626e2d7e4f
Valerii <valerabulon@gmail.com>
2024-05-10 13:38:03 +0300

    add loading effects for web and images pages

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -9,7 +9,7 @@ export default async function WebSearchPage({ searchParams }) {
   if (!response.ok) throw new Error("Something went wrong");
   const data = await response.json();
   const results = data.items;
-
+  console.log(response.status);
   if (!results) {
     return (
       <div className="flex flex-col justify-center items-center pt-10">
